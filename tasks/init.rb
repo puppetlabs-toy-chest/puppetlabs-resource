0d000ec97a39ecf1f414c4e5ba9bd235bde196f8
Hunter Haugen <hunter@puppet.com>
2017-09-25 10:36:58 -0700

    (maint) Fetch properties
    The provider must be turned into a resource via .to_resource before the
    properties will be printed by .to_hash

--- tasks/init.rb
+++ tasks/init.rb
@@ -5,16 +5,16 @@ require 'json'
 
 def get(type, name)
   output_instances = []
+  opts = { name: name } unless name.nil?
+  type = Puppet::Type.type(type.to_sym)
+  instances = type.instances
   if name.nil?
-    opts = { name: name } unless name.nil?
-    provider = Puppet::Type.type(type.to_sym)
-    provider.instances.each do |instance|
-      output_instances.push(instance.to_hash)
+    instances.each do |instance|
+      output_instances.push(instance.to_resource.to_hash)
     end
   else
-    opts = { name: name }
-    instance = Puppet::Type.type(type.to_sym).new(opts)
-    output_instances.push(instance.to_hash)
+    res = instances.find { |i| i[:name] == name }
+    output_instances.push(res.to_resource.to_hash) if res
   end
 
   result = output_instances.to_json
