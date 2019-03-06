c8a174b3eef7f67b2fd4a0d9577db2bf56bb567e
Eimhin Laverty <eimlav@users.noreply.github.com>
2019-03-06 16:04:22 +0000

    (MODULES-8717) Fix dependency issue with BoltSpec (#43)
    * (MODULES-8717) Fix dependency issue with BoltSpec
    * Change minimum Ruby version for Bolt gem
    * Remove unnecessary inventory config
    * Update README to account for Bolt requirements

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -7,15 +7,22 @@ username = if fact('operatingsystem') == 'windows'
              'root'
            end
 
-describe 'resource task', if: puppet_version =~ %r{(5\.\d\.\d)} && !UNSUPPORTED_PLATFORMS.include?(fact('operatingsystem')) do
+describe 'resource task' do
+  include Beaker::TaskHelper::Inventory
+  include BoltSpec::Run
+
   describe 'resource' do
     it 'get a single instance' do
-      result = run_task(task_name: 'resource', params: "type=user name=#{username}")
-      expect_multiple_regexes(result: result, regexes: [%r{name.*:.*"#{username}"}, %r{Job completed. 1/1 nodes succeeded|Ran on 1 node}])
+      result = task_run('resource', 'type' => 'user', 'name' => username.to_s)
+
+      expect(result.first['status']).to eq 'success'
+      expect(result.first['result']['_output']).to match %r{name.*:.*"#{username}"}
     end
     it 'get all instances' do
-      result = run_task(task_name: 'resource', params: 'type=user')
-      expect_multiple_regexes(result: result, regexes: [%r{.*name.*"#{username}"}, %r{Job completed. 1/1 nodes succeeded|Ran on 1 node}])
+      result = task_run('resource', 'type' => 'user')
+
+      expect(result.first['status']).to eq 'success'
+      expect(result.first['result']['_output']).to match %r{name.*:.*"#{username}"}
     end
   end
 end
